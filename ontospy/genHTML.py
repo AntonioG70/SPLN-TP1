64b83655542977161125e494d35839213f8f536d
AntonioG70 <48025387+AntonioG70@users.noreply.github.com>
2021-05-09 15:34:26 +0100

    coragem

--- ontospy/genHTML.py
+++ ontospy/genHTML.py
@@ -4,8 +4,8 @@ from ontospy.ontodocs.viz.viz_html_single import *
 #documentação da ontologia
 g = ontospy.Ontospy("Periodic_Table.rdf")
 
-v = HTMLVisualizer(g) # => instantiate the visualization object
-v.build() # => render visualization. You can pass an 'output_path' parameter too
-v.preview() # => open in browser
+v = HTMLVisualizer(g) 
+v.build() 
+v.preview() 
 
 #ontospy gendocs pubs.ttl
